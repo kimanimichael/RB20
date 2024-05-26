21321751ec8f41cf75e22183434267d3ec9b0a0e
MK <muchunumike@gmail.com>
2024-05-26 14:21:40 +0300

    f(scale_certification): Use _ for class members

--- include/math_utils/pid/pid.h
+++ include/math_utils/pid/pid.h
@@ -4,26 +4,26 @@ class PID
 {
 
 public:
-    PID(/* args */);
+    PID(double kp, double ki, double kd);
     // ~pid();
 public:
    
     double output;
+    double _dt;
 
     double calculateOutput(double setPoint, double processVariable);
 
 private:
     /* data */
-    double _dt;
     double _setPoint;
     double _processVariable;
-    double error;
-    double previousError;
-    double integral;
-    double derivative;
-    double kp;
-    double ki;
-    double kd;
+    double _error;
+    double _previousError;
+    double _integral;
+    double _derivative;
+    double _kp;
+    double _ki;
+    double _kd;
 
 };
 
