8eea2061e49ef205275034dbcbc0c5bfcddeb8b5
MK <muchunumike@gmail.com>
2024-05-26 14:28:52 +0300

    f(scale_certification): Check the status of the car and calculate the appropriate PID output

---
+++ include/apps/car_app/car_line_following_pid.h
@@ -0,0 +1,13 @@
+#ifndef __CAR_LF__PID
+#define __CAR_LF__PID
+
+#include "pid.h"
+#include "bsp.h"
+
+extern double PIDOutput;
+
+void carAppInit();
+
+void carAppRun();
+
+#endif
---
+++ main/apps/car_app/car_line_following_pid.cpp
@@ -0,0 +1,42 @@
+#include "car_line_following_pid.h"
+
+Infrared5Channel* infraredSensor;
+Infrared5Channel::detectedChannels sensorDetectedChannels;
+
+double lineError;
+PID lineFollowingPid(1.00, 1.00, 1.00);
+double PIDOutput;
+
+void carAppInit() {
+    infraredSensor = BSP::getDefaultInfraredSensor();
+    carAppRun();
+}
+
+void carAppRun() {
+    sensorDetectedChannels = infraredSensor->read();
+    switch (sensorDetectedChannels)
+    {
+    case Infrared5Channel::detectedChannels::centralChannel:
+        lineError = 0.0;
+        break;
+    case Infrared5Channel::detectedChannels::halfLeftChannel:
+        lineError = -1.0;
+        break;
+    case Infrared5Channel::detectedChannels::farLeftChannel:
+        lineError = -2.0;
+        break;
+    case Infrared5Channel::detectedChannels::halfRightChannel:
+        lineError = 1.0;
+        break;
+    case Infrared5Channel::detectedChannels::farRightChannel:
+        lineError = 2.0;
+        break;
+    default:
+        break;
+    }
+
+    lineFollowingPid._dt = 0.025;
+    PIDOutput = lineFollowingPid.calculateOutput(0, lineError);
+
+
+}
