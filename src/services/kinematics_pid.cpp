13201ba3bd8d30fa03f8dff3972f3decaf733df0
MK <muchunumike@gmail.com>
2024-06-08 01:41:39 +0300

    f(f/pid_for_speed_control): Set motor speeds based on PID output

--- src/services/kinematics_pid.cpp
+++ src/services/kinematics_pid.cpp
@@ -1,16 +1,32 @@
 #include "kinematics_pid.h"
+#include <stdio.h>
 #include "bsp.h"
 
 BSP::DcMotor* kinematicsLeftMotor = BSP::getDefaultKinematicsLeftMotor();
 BSP::DcMotor* kinematicsRightMotor = BSP::getDefaultKinematicsRightMotor();
 
+#define LEFT_MOTOR_BASE_SPEED 79
+#define RIGHT_MOTOR_BASE_SPEED 78
+// #define RIGHT_MOTOR_MAX_SPEED 79
+
+
 unsigned int leftMotorSpeed;
-unsigned int RightMotorSpeed;
+unsigned int rightMotorSpeed;
 
-void controlCarKinematics() {
-    leftMotorSpeed = PIDOutput * 2;
-    RightMotorSpeed = PIDOutput / 2;
+double controlCarKinematics() {
 
+    leftMotorSpeed = LEFT_MOTOR_BASE_SPEED - PIDOutput;
+    rightMotorSpeed = RIGHT_MOTOR_BASE_SPEED + PIDOutput;
+    // if (rightMotorSpeed > RIGHT_MOTOR_MAX_SPEED) {
+    //     rightMotorSpeed = RIGHT_MOTOR_MAX_SPEED;
+    // }
+
+    // printf("Left Motor: %u || Right Motor: %u\n", leftMotorSpeed, rightMotorSpeed);
+
+    kinematicsLeftMotor->set_dir(BSP::CLOCKWISE);
     kinematicsLeftMotor->set_raw_speed(leftMotorSpeed);
-    kinematicsRightMotor->set_raw_speed(RightMotorSpeed);
+
+    kinematicsRightMotor->set_dir(BSP::CLOCKWISE);
+    kinematicsRightMotor->set_raw_speed(rightMotorSpeed);
+    return PIDOutput;
 }
