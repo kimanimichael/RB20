9d065e972540963d696f5a058b49eb9182d8291d
MK <muchunumike@gmail.com>
2024-11-04 01:10:49 +0300

    f(multiplatform): Add common files and dirs to cmake

---
+++ cmake/app.cmake
@@ -0,0 +1,23 @@
+cmake_minimum_required(VERSION 3.16 FATAL_ERROR)
+
+set(APP_COMMON_INCLUDE_DIRS
+        "${CMAKE_SOURCE_DIR}/include"
+
+        "${CMAKE_SOURCE_DIR}/include/bsp"
+        "${CMAKE_SOURCE_DIR}/include/bsp/drivers/"
+        "${CMAKE_SOURCE_DIR}/include/bsp/drivers/actuators"
+        "${CMAKE_SOURCE_DIR}/include/bsp/drivers/sensors"
+
+        "${CMAKE_SOURCE_DIR}/include/apps/car_app"
+
+        "${CMAKE_SOURCE_DIR}/include/services/kinematics"
+        "${CMAKE_SOURCE_DIR}/include/services/steering"
+        "${CMAKE_SOURCE_DIR}/include/services/car_controller"
+)
+
+set(APP_COMMON_SOURCES
+        "${CMAKE_SOURCE_DIR}/src/RB19.cpp"
+        "${CMAKE_SOURCE_DIR}/src/apps/car_app/car_line_following.cpp"
+        "${CMAKE_SOURCE_DIR}/src/services/kinematics.cpp"
+        "${CMAKE_SOURCE_DIR}/src/services/steering.cpp"
+)
