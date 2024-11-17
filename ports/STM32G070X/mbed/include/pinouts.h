3731e0dee0dd7da3f962b5ca9d9104e87e9f3e39
MK <muchunumike@gmail.com>
2024-11-18 00:50:20 +0300

    f(multiplatform): Configure G070 mbed pinouts

---
+++ ports/STM32G070X/mbed/include/pinouts.h
@@ -0,0 +1,13 @@
+#ifndef MBED_PINOUT_H
+#define MBED_PINOUT_H
+
+#define MBED_G070_ONBOARD_LED         PB_3
+
+#define MBED_G070_FTSMC_MOTOR_PWM         PB_3
+
+#define MBED_G070_IR_FAR_LEFT          PB_4
+#define MBED_G070_IR_CLOSE_LEFT          PB_4
+#define MBED_G070_IR_MIDDLE          PB_4
+#define MBED_G070_IR_CLOSE_RIGHT          PB_4
+#define MBED_G070_IR_FAR_RIGHT          PB_4
+#endif
